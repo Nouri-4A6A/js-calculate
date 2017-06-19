2f7cf90290906c5f341e5fe018b56e7a4ff4c9ac
BolleNicht <512576@edu.rocmn.nl>
2017-06-19 19:12:31 +0200

    Add files via upload

---
+++ js-calculate_script.js
@@ -0,0 +1,36 @@
+function optellen(){
+	var array;
+	var words = document.getElementById('getallen').value;
+	array = (words.split(","));
+	var operator = document.getElementById('keuze').value;
+
+ 
+	for(i=0; i <array.length; i++){
+		 array[i] = parseFloat(array[i]);
+
+	}
+
+	var result = array[0];
+	for(i=1; i<array.length; i++){
+		result = berekenen(result,array[i],operator);
+
+	}
+
+	function berekenen(nummer,nummer2,operator){
+		if (operator == '+') {
+            result = nummer + nummer2;
+
+        } else if (operator == '-') {
+            result = nummer - nummer2;
+
+        } else if (operator == '*') {
+            result = nummer * nummer2;
+
+        } else if (operator == '/') {
+            result = nummer / nummer2;
+        }
+        return result;
+    }
+    console.log(result);
+    document.getElementById('result').innerHTML = "result:" + result;
+}
